57034315d4cbe42fd00104b45e732b9f74082dc0
Cody Lee <codylee02@gmail.com>
2019-11-26 20:37:51 -0700

    prep for production

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   // other stuff
   API_BASE_URL:
-    process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/api",
+    process.env.REACT_APP_API_BASE_URL || "https://immense-atoll-77292.herokuapp.com/api",
   API_KEY: process.env.REACT_APP_API_KEY
 };
