8208c3325d1f289b20ef85ff5bc3442709367d39
Cody Lee <codylee02@gmail.com>
2019-11-27 21:40:49 -0700

    made dev env

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   // other stuff
-  API_BASE_URL: "https://immense-atoll-77292.herokuapp.com/api",
+  API_BASE_URL: process.env.NODE_ENV === 'development' ? process.env.REACT_APP_API_BASE_URL : "https://immense-atoll-77292.herokuapp.com/api",
   API_KEY: process.env.REACT_APP_API_KEY
 };
