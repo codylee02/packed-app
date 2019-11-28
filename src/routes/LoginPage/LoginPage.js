823f7212f49cfc5d4bc6bb28cc4925be2ba4cf47
Cody Lee <codylee02@gmail.com>
2019-11-27 20:58:35 -0700

    prerp for deploy

--- src/routes/LoginPage/LoginPage.js
+++ src/routes/LoginPage/LoginPage.js
@@ -2,20 +2,20 @@ import React from "react";
 import LoginForm from "../../components/LoginForm/LoginForm";
 import { Section } from "../../components/Utils/Utils";
 
-export default function LoginPage(props) {
-  function handleLoginSuccess() {
-    const { location, history } = this.props;
-    const destination = (location.state || {}).from || "/lists";
-    history.push(destination);
+export default class LoginPage extends React.Component {
+  handleLoginSuccess = () => {
+    const { location, history } = this.props
+    const destination = (location.state || {}).from || '/lists'
+    history.push(destination)
   }
-
+render() {
   return (
     <Section className="LoginPage">
       <h2>Login</h2>
-      <LoginForm onLoginSuccess={handleLoginSuccess} />
+      <LoginForm onLoginSuccess={this.handleLoginSuccess} />
     </Section>
   );
-}
+}}
 
 LoginPage.defaultProps = {
   location: {},
