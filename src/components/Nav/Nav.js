0edd441eac612ee5ebdeac5677049c0b0604f8db
Cody Lee <codylee02@gmail.com>
2019-11-27 17:44:33 -0700

    added logout

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import TokenService from "../../services/token-service";
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faList, faCopy } from "@fortawesome/free-solid-svg-icons";
+import {
+  faList,
+  faCopy,
+  faSignOutAlt
+} from "@fortawesome/free-solid-svg-icons";
 
 import "./Nav.css";
 
 export default function Nav(props) {
+  function handleLogoutClick() {
+    TokenService.clearAuthToken();
+  }
+
   return (
     <nav className="Nav">
       <Link to={"/lists"} className="nav-buttons">
@@ -20,6 +30,12 @@ export default function Nav(props) {
           <FontAwesomeIcon icon={faCopy} className="nav-icons" />
         </div>
         <div className="nav-label">Templates</div>
+      </Link>
+      <Link onClick={handleLogoutClick} to="/" className="nav-buttons">
+        <div className="nav-icon">
+          <FontAwesomeIcon icon={faSignOutAlt} className="nav-icons" />
+        </div>
+        <div className="nav-label">Logout</div>
       </Link>
     </nav>
   );
