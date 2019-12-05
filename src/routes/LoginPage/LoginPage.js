c151c4fc3d3808f7930a536e628d1c80cbaa0e0e
Cody Lee <codylee02@gmail.com>
2019-12-04 21:39:36 -0700

    added styling

--- src/routes/LoginPage/LoginPage.js
+++ src/routes/LoginPage/LoginPage.js
@@ -2,20 +2,23 @@ import React from "react";
 import LoginForm from "../../components/LoginForm/LoginForm";
 import { Section } from "../../components/Utils/Utils";
 
+import "./LoginPage.css";
+
 export default class LoginPage extends React.Component {
   handleLoginSuccess = () => {
-    const { location, history } = this.props
-    const destination = (location.state || {}).from || '/lists'
-    history.push(destination)
+    const { location, history } = this.props;
+    const destination = (location.state || {}).from || "/lists";
+    history.push(destination);
+  };
+  render() {
+    return (
+      <Section className="LoginPage">
+        <h2>Login</h2>
+        <LoginForm onLoginSuccess={this.handleLoginSuccess} />
+      </Section>
+    );
   }
-render() {
-  return (
-    <Section className="LoginPage">
-      <h2>Login</h2>
-      <LoginForm onLoginSuccess={this.handleLoginSuccess} />
-    </Section>
-  );
-}}
+}
 
 LoginPage.defaultProps = {
   location: {},
