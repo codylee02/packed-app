4ab2617d8367d4e2466b6ff966e328f25b0781b6
Cody Lee <codylee02@gmail.com>
2019-12-04 21:36:59 -0700

    added cancel button and styling

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -2,15 +2,14 @@ import React, { Component } from "react";
 import { Button, Input, Required } from "../Utils/Utils";
 import AuthApiService from "../../services/auth-api-service";
 import TokenService from "../../services/token-service";
+import { Link } from "react-router-dom";
+
+import "./RegistrationForm.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 export default class RegistrationForm extends Component {
-  static defaultProps = {
-    onRegistrationSuccess: () => {}
-  };
-
   state = { error: null, registering: false };
 
   handleSubmit = ev => {
@@ -45,57 +44,73 @@ export default class RegistrationForm extends Component {
       this.state.registering === false ? (
         <Button type="submit">Register</Button>
       ) : (
-        <FontAwesomeIcon icon={faSpinner} spin />
+        <Button>
+          <FontAwesomeIcon icon={faSpinner} spin />
+        </Button>
       );
     return (
       <form className="RegistrationForm" onSubmit={this.handleSubmit}>
         <div role="alert">{error && <p className="red">{error}</p>}</div>
-        <div className="first_name">
-          <label htmlFor="RegistrationForm__first_name">
-            First name <Required />
-          </label>
-          <Input
-            name="first_name"
-            type="text"
-            required
-            id="RegistrationForm__first_name"
-          ></Input>
+
+        <label
+          className="RegistrationForm__label"
+          htmlFor="RegistrationForm__first_name"
+        >
+          First name <Required />
+        </label>
+        <Input
+          className="RegistrationForm__input"
+          name="first_name"
+          type="text"
+          required
+          id="RegistrationForm__first_name"
+        ></Input>
+
+        <label
+          className="RegistrationForm__label"
+          htmlFor="RegistrationForm__last_name"
+        >
+          Last name <Required />
+        </label>
+        <Input
+          className="RegistrationForm__input"
+          name="last_name"
+          type="text"
+          required
+          id="RegistrationForm__last_name"
+        ></Input>
+        <label
+          className="RegistrationForm__label"
+          htmlFor="RegistrationForm__username"
+        >
+          Email <Required />
+        </label>
+        <Input
+          className="RegistrationForm__input"
+          name="username"
+          type="email"
+          required
+          id="RegistrationForm__username"
+        ></Input>
+        <label
+          className="RegistrationForm__label"
+          htmlFor="RegistrationForm__password"
+        >
+          Password <Required />
+        </label>
+        <Input
+          className="RegistrationForm__input"
+          name="password"
+          type="password"
+          required
+          id="RegistrationForm__password"
+        ></Input>
+        <div className="RegistrationForm__form-controls">
+          <Link to={"/"}>
+            <Button type="button">Cancel</Button>
+          </Link>
+          {registerButton}
         </div>
-        <div className="last_name">
-          <label htmlFor="RegistrationForm__last_name">
-            Last name <Required />
-          </label>
-          <Input
-            name="last_name"
-            type="text"
-            required
-            id="RegistrationForm__last_name"
-          ></Input>
-        </div>
-        <div className="username">
-          <label htmlFor="RegistrationForm__username">
-            Email
-            <Required />
-          </label>
-          <Input
-            name="username"
-            type="email"
-            required
-            id="RegistrationForm__username"
-          ></Input>
-        </div>
-        <div className="password">
-          <label htmlFor="RegistrationForm__password">
-            Password <Required />
-          </label>
-          <Input
-            name="password"
-            type="password"
-            required
-            id="RegistrationForm__password"
-          ></Input>
-        </div>
-        {registerButton}
       </form>
     );
   }
